ba55041115ce4d2aff01fad72d2778adf58e0052
Thrauk <stoicath@gmail.com>
2021-12-30 18:02:37 +0200

    revamp structure and firebase analytics removal

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:union_app/src/repository/storage/firebase_storage/firebase_storage_repository.dart';
-import 'repository/authentication/auth.dart';
+import 'src/repository/authentication/auth.dart';
 
-import 'screens/app/app.dart';
+import 'src/screens/app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
