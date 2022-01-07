eb879f34a124a9dcfc3f561292f8161aade85650
Thrauk <stoicath@gmail.com>
2022-01-07 19:02:38 +0200

    projects on profile will now take you to the project page

--- lib/src/screens/profile/widgets/projects/project_list_element_widget.dart
+++ lib/src/screens/profile/widgets/projects/project_list_element_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:union_app/src/models/models.dart';
 import 'package:union_app/src/screens/home/home.dart';
+import 'package:union_app/src/screens/project/project_details/view/project_details_page.dart';
 
 import '../../../../theme.dart';
 
@@ -16,61 +17,73 @@ class ProjectListElementWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.backgroundLight,
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              _project.title!,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontFamily: 'Lato',
-                fontSize: 18,
-                color: AppColors.white09,
-              ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => ProjectDetailsPage(
+              project: _project,
             ),
+          ),
+        );
+      },
+      child: Card(
+        color: AppColors.backgroundLight,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                _project.title!,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 18,
+                  color: AppColors.white09,
+                ),
+              ),
 
-            const SizedBox(height: 12),
-            Wrap(
-              children: [
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    minHeight: 14,
-                  ),
-                  child: GestureDetector(
-                    child: Text(
-                      _project.shortDescription,
-                      style: const TextStyle(
-                        fontFamily: 'Lato',
-                        fontSize: 14,
-                        color: AppColors.white07,
+              const SizedBox(height: 12),
+              Wrap(
+                children: [
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      minHeight: 14,
+                    ),
+                    child: GestureDetector(
+                      child: Text(
+                        _project.shortDescription,
+                        style: const TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 14,
+                          color: AppColors.white07,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 8,
-            ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
 
-            Row(
-              children: <Widget>[
-                Avatar(photo: _user.photo, avatarSize: 10,),
-                const SizedBox(width: 10),
-                Text(
-                  'Created by ${_user.displayName}',
-                  overflow: TextOverflow.ellipsis,
-                  style: AppStyles.textStyleBodySmall,
-                ),
-                const Spacer(),
-              ],
-            ),
-          ],
+              Row(
+                children: <Widget>[
+                  Avatar(photo: _user.photo, avatarSize: 10,),
+                  const SizedBox(width: 10),
+                  Text(
+                    'Created by ${_user.displayName}',
+                    overflow: TextOverflow.ellipsis,
+                    style: AppStyles.textStyleBodySmall,
+                  ),
+                  const Spacer(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
