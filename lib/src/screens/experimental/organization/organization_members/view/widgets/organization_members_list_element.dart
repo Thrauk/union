563e13cc35cacda76e5d9070aae1409d52584425
Amalia Muresan <amaliamuresan1290@gmail.com>
2022-01-25 18:26:22 +0200

    changes to organization members list

--- lib/src/screens/experimental/organization/organization_members/view/widgets/organization_members_list_element.dart
+++ lib/src/screens/experimental/organization/organization_members/view/widgets/organization_members_list_element.dart
@@ -24,53 +24,99 @@ class OrganizationMembersListElement extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Avatar(
-                photo: user.photo,
-                avatarSize: 24,
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Flexible(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      user.displayName ?? '',
-                      style: AppStyles.textStyleBody,
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Avatar(
+                    photo: user.photo,
+                    avatarSize: 24,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Flexible(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          user.displayName ?? '',
+                          style: AppStyles.textStyleBody,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          user.jobTitle ?? '',
+                          style: AppStyles.textStyleBodySmall,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
                     ),
-                    Text(
-                      user.jobTitle ?? '',
-                      style: AppStyles.textStyleBodySmall,
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               if (loggedUid != user.id)
                 Align(
                   alignment: Alignment.centerRight,
                   child: GestureDetector(
-                      onTap: () {
-                        context.read<OrganizationMembersBloc>().add(RemoveMember(memberUid: user.id));
-                      },
-                      child: const Icon(
-                        Icons.remove_circle,
-                        color: AppColors.primaryColor,
-                      )),
-                )
-              else
-                const Align(
-                  alignment: Alignment.centerRight,
-                  child: Icon(
-                    Icons.blender,
-                    color: AppColors.primaryColor,
+                    onTap: () {
+                      showDeleteDialog(context, user);
+                      // context.read<OrganizationMembersBloc>().add(RemoveMember(memberUid: user.id));
+                    },
+                    child: const Icon(
+                      Icons.person_remove_alt_1,
+                      color: AppColors.redLight,
+                    ),
                   ),
-                ),
+                )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+void showDeleteDialog(BuildContext context, FullUser user) {
+  showDialog(
+    context: context,
+    builder: (BuildContext dialogContext) => Center(
+      child: AlertDialog(
+        elevation: 20,
+        actionsPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        actions: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: AppColors.primaryColor,
+            ),
+            onPressed: () {
+              context.read<OrganizationMembersBloc>().add(RemoveMember(memberUid: user.id));
+              Navigator.of(dialogContext).pop();
+            },
+            child: Text(
+              'Yes',
+              style: AppStyles.textStyleBody.merge(
+                const TextStyle(color: AppColors.backgroundLight, fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          GestureDetector(
+            onTap: () => Navigator.of(dialogContext).pop(),
+            child: Text(
+              'No',
+              style: AppStyles.textStyleBody.merge(
+                const TextStyle(color: AppColors.redLight),
+              ),
+            ),
+          ),
+        ],
+        backgroundColor: AppColors.backgroundLight,
+        title: const Text('Hold on!', style: AppStyles.textStyleHeading1),
+        content:
+            Text('Are you sure you want remove ${user.displayName} from the organization?', style: AppStyles.textStyleBody),
+      ),
+    ),
+  );
 }
